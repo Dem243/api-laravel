50e4ae1102c1e122d1e4b5499e222de0b3053108
Dem243 <raphamutombo707@gmail.com>
2026-09-19 13:45:43 +0100

    learn: implement nested resources for project tasks

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreTaskRequest;
+use App\Http\Requests\UpdateTaskRequest;
+use App\Models\Task;
+use App\Traits\ApiResponseTrait;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    use ApiResponseTrait;
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreTaskRequest $request)
+    {
+
+        $task = Task::create($request->validated());
+
+        return $this->successResponse($task, 'Taches créés avec succés');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(UpdateTaskRequest $task)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Task $task)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Task $task)
+    {
+        //
+    }
+}
